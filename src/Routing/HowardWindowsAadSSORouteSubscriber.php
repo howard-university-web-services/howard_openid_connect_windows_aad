4b2a21a63546d894319108093cae04d9ab53ae6d
Dan Rogers <dan.rogers@idfive.com>
2025-05-02 13:45:41 -0600

    11x prep

--- src/Routing/HowardWindowsAadSSORouteSubscriber.php
+++ src/Routing/HowardWindowsAadSSORouteSubscriber.php
@@ -25,9 +25,6 @@ class HowardWindowsAadSSORouteSubscriber extends RouteSubscriberBase {
         // Not important to differentiate between Exceptions here, we just need
         // make it know that something is wrong and we won't enable SSOut.
         $configuration = FALSE;
-        // TODO: When watchdog_exception() is deprecated and ExceptionLogger is
-        // available, update this to use ExceptionLogger.
-        // @see https://www.drupal.org/project/drupal/issues/2932518
         $variables = Error::decodeException($exception);
         \Drupal::logger('howard_openid_connect_windows_aad')->error('Failed to check OpenID Connect Windows AAD configuration so Single Sign Off will remain disabled. %type: @message in %function (line %line of %file).', $variables);
       }
